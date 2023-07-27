f02026698da5d0940b4d36270ba18029b52cbc74
lesponns <adamadakadam1903@gmail.com>
2023-07-27 21:15:54 +0300

    Add project files.

---
+++ SmartPro_13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int last=0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i+" ");
+                last = last +i;
+            }
+            Console.WriteLine("Sonuç: "+last);
+            Console.ReadKey();
+        }
+    }
+}
